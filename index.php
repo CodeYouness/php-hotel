a72a1efd8edc72c4145d1d974118a426f14fe56e
CodeYouness <ylijassi@gmail.com>
2024-11-05 10:16:58 +0100

    minority fix to style

--- index.php
+++ index.php
@@ -38,7 +38,6 @@ if ($hotelvalue > 0) {
     $secondfilteredhotels = $filteredhotels;
 }
 
-var_dump($hotelvalue);
 ?>
 
 <!DOCTYPE html>
@@ -47,17 +46,21 @@ var_dump($hotelvalue);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="./style/style.css">
     <title>Hotels</title>
 </head>
 
 <body>
 
+    <header>
+        <h1 class="text-center">Hotel Finder</h1>
+    </header>
 
     <main class="container">
 
-        <form action="./index.php" method="get">
+        <form action="./index.php" method="get" class="mb-3">
             <label for="parking">Parking:</label>
             <select name="parking" id="parking">
                 <option value="default">Default</option>
@@ -78,16 +81,17 @@ var_dump($hotelvalue);
 
         <div class="row d-flex">
             <?php foreach ($secondfilteredhotels as $hotel) { ?>
-                <div class="col-4 mb-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title text-center mb-5"><?php echo $hotel['name'] ?></h5>
-                            <h6 class="card-subtitle mb-2 text-body-secondary">Vote: <?php echo $hotel['vote'] ?></h6>
-                            <p class="card-text"><?php echo $hotel['description'] ?> at <?php echo $hotel['distance_to_center'] ?>KM from you</p>
-                            <a href="#" class="card-link">Hotel link</a>
-                        </div>
+            <div class="col-4 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title text-center mb-5"><?php echo $hotel['name'] ?></h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Vote: <?php echo $hotel['vote'] ?></h6>
+                        <p class="card-text"><?php echo $hotel['description'] ?> at
+                            <?php echo $hotel['distance_to_center'] ?>KM from you</p>
+                        <a href="#" class="card-link">Hotel link</a>
                     </div>
                 </div>
+            </div>
             <?php } ?>
         </div>
     </main>
